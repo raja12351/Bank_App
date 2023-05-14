018f9f5cd093d3c397a31561fa346cca2575f634
raja12351 <rajatsingh12351@gmail.com>
2023-05-14 22:51:05 +0530

    "updated"

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        HDFCBankAccount obj = new HDFCBankAccount(500);
+        System.out.println(obj.getAccountNumber());
+        obj.addMoney(300);
+        System.out.println(obj.checkBalance());
+        obj.withdrawMoney(200);
+        System.out.println(obj.getROI());
+        System.out.println(obj.getTotalInterest(500,2));
+        System.out.println(obj.checkBalance());
     }
 }
